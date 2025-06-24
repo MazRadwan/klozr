b7903b62687958ad8bc0cc02971455a0c0f5d82a
Maz Radwan <mazradwan@Mazs-MacBook-Air.local>
2025-06-24 19:36:24 -0230

    added functionality to quick action button in company detail page

--- src/app/dashboard/contacts/[id]/page.tsx
+++ src/app/dashboard/contacts/[id]/page.tsx
@@ -1256,7 +1256,7 @@ export default function ContactDetailPage() {
                       disabled={activityLoading}
                       className="flex-1 text-gray-700 dark:text-gray-300 hover:bg-green-50 hover:text-green-600 dark:hover:bg-green-900/20 dark:hover:text-green-400 hover:border-green-200 dark:hover:border-green-800"
                     >
-                      <Phone className="h-4 w-4" />
+                      <Phone className="h-5 w-5" />
                     </Button>
                   </Tooltip>
 
@@ -1268,7 +1268,7 @@ export default function ContactDetailPage() {
                       disabled={activityLoading}
                       className="flex-1 text-gray-700 dark:text-gray-300 hover:bg-blue-50 hover:text-blue-600 dark:hover:bg-blue-900/20 dark:hover:text-blue-400 hover:border-blue-200 dark:hover:border-blue-800"
                     >
-                      <Mail className="h-4 w-4" />
+                      <Mail className="h-5 w-5" />
                     </Button>
                   </Tooltip>
 
@@ -1280,7 +1280,7 @@ export default function ContactDetailPage() {
                       disabled={activityLoading}
                       className="flex-1 text-gray-700 dark:text-gray-300 hover:bg-gray-50 hover:text-gray-600 dark:hover:bg-gray-900/20 dark:hover:text-gray-400 hover:border-gray-200 dark:hover:border-gray-800"
                     >
-                      <FileText className="h-4 w-4" />
+                      <FileText className="h-5 w-5" />
                     </Button>
                   </Tooltip>
 
@@ -1292,7 +1292,7 @@ export default function ContactDetailPage() {
                       disabled={activityLoading}
                       className="flex-1 text-gray-700 dark:text-gray-300 hover:bg-purple-50 hover:text-purple-600 dark:hover:bg-purple-900/20 dark:hover:text-purple-400 hover:border-purple-200 dark:hover:border-purple-800"
                     >
-                      <Calendar className="h-4 w-4" />
+                      <Calendar className="h-5 w-5" />
                     </Button>
                   </Tooltip>
 
@@ -1304,7 +1304,7 @@ export default function ContactDetailPage() {
                       disabled={activityLoading}
                       className="flex-1 text-gray-700 dark:text-gray-300 hover:bg-orange-50 hover:text-orange-600 dark:hover:bg-orange-900/20 dark:hover:text-orange-400 hover:border-orange-200 dark:hover:border-orange-800"
                     >
-                      <CheckSquare className="h-4 w-4" />
+                      <CheckSquare className="h-5 w-5" />
                     </Button>
                   </Tooltip>
                 </div>
