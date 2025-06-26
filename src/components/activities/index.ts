da78b882fee091a831ff40dd1f621ee2eb81a440
Maz Radwan <mazradwan@Mazs-MacBook-Air.local>
2025-06-26 20:31:13 -0230

    foundation for refactor

--- src/components/activities/index.ts
+++ src/components/activities/index.ts
@@ -1,5 +1,6 @@
 // Barrel file for activities components
 export { ActivityFeed } from './ActivityFeed';
 export { ActivityCard } from './ActivityCard';
+export { ActivityIcon } from './ActivityIcon';
 export { QuickActions } from './QuickActions';
 export { CreateActivityModal } from './CreateActivityModal';
